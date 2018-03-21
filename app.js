0e3c733923e0e8fd4d04fb1911f950414924a215
Le Trung Kien <kientrungle2001@gmail.com>
2018-03-21 14:03:23 +0700

    all

--- app.js
+++ app.js
@@ -6,19 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "dbfull5"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  
-});
   
 var index = require('./routes/index');
 var users = require('./routes/users');
